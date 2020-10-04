6fe2d20b360f71f1e8887e3dd9fb3ab1a0deeaa4
Ravindra Singh <58778755+ravindra-code@users.noreply.github.com>
2020-10-04 22:18:06 +0530

    Price List

---
+++ src/main/java/data/Price.java
@@ -0,0 +1,44 @@
+package data;
+
+public enum Price {
+
+    A("A",50),
+    B("B",30),
+    C("C",20),
+    D("D", 15);
+
+
+    private final String item;
+    private final double cost;
+
+    Price(String item, double cost){
+        this.cost = cost;
+        this.item = item;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public static Price getEnum(String key){
+
+        switch (key){
+            case "A":return Price.A;
+
+            case "B": return Price.B;
+
+            case "C": return Price.C;
+
+            case "D": return Price.D;
+
+            default: throw new IllegalArgumentException ("Item not found.");
+        }
+    }
+
+
+
+}
